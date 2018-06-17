804d5917745b61d3a1f0aba624dbd59e442ad8a2
Glitch (cyber-koala) <none>
2018-06-17 08:08:21 +0000

    🙈👏 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+// require and use "multer"...
+const multer = require('multer');
+const upload = multer();
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.get('/hello', function(req, res){
+  res.json({greetings: "Hello, API"});
+});
+
+// Use multer middleware
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res, next) {
+  // Respond with file metadata
+  res.send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    syze: req.file.size
+  });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
